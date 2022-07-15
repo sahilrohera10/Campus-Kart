24342d8f575e1fe79510c38e21ef5caac2e42ac8
Sahil <sahilrohera10@gmail.com>
2022-07-15 20:43:57 +0530

    origin set

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3999;
 
 const cors = require("cors");
 
