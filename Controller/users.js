4a3f0c43715620b5f228eae1115237b66d3e44a5
Sahil <sahilrohera10@gmail.com>
2022-07-17 23:12:14 +0530

    removed otp check

--- Controller/users.js
+++ Controller/users.js
@@ -89,16 +89,16 @@ async function sendMAil(req, res, next) {
   try {
     const val = Math.floor(1000 + Math.random() * 9000);
     const user = await otps.find({ emailid: req.body.Email });
-    if (!user) {
-      await otps.create({
-        emailid: req.body.Email,
-        otp: val,
-      });
-      console.log("here");
-    } else {
-      await otps.updateOne({ emailid: req.body.Email }, { otp: val });
-      console.log("there");
-    }
+    // if (!user) {
+    await otps.create({
+      emailid: req.body.Email,
+      otp: val,
+    });
+    console.log("here");
+    // } else {
+    //   await otps.updateOne({ emailid: req.body.Email }, { otp: val });
+    //   console.log("there");
+    // }
 
     const tranporter = nodemailer.createTransport({
       service: "gmail",
